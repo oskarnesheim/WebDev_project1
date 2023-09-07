0fe9e95c514d8b849ca5829ca1eecfcaff63ffee
Andreas Kjosavik <andrkjos@stud.ntnu.no>
2023-09-07 09:14:56 +0200

    feat: add favorite for cities

--- src/pages/City.tsx
+++ src/pages/City.tsx
@@ -1,17 +1,54 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import { useState } from "react";
 import { NavLink, Outlet, useLoaderData } from "react-router-dom";
 
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 function City() {
   const city: string = useLoaderData() as string;
+
+  const [favorite, setFavorite] = useState(getCityStatus(city));
+
+  function getCityStatus(cityName: string) {
+    const favorites: string[] = getFavorites();
+    console.log(favorites);
+
+    if (favorites.includes(cityName)) {
+      return "★";
+    }
+    return "☆";
+  }
+
+  function getFavorites() {
+    let favorites: string[] = [];
+    if (localStorage.getItem("favorites")) {
+      favorites = JSON.parse(localStorage.getItem("favorites")!);
+    }
+    return favorites;
+  }
+
+  function toggleFavorite() {
+    const favorites: string[] = getFavorites();
+    if (favorites.includes(city)) {
+      favorites.splice(favorites.indexOf(city), 1);
+    } else {
+      favorites.push(city);
+    }
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+    return setFavorite(getCityStatus(city));
+  }
+
   return (
     <div>
-      <h2>Welcome to {city}</h2>
+      <h2>
+        Welcome to {city}
+        {favorite}
+      </h2>
       <nav>
         <NavLink to={`current`}>Current</NavLink>
         <NavLink to={`forecast`}>Forecast</NavLink>
       </nav>
+      <button onClick={toggleFavorite}>Toggle favorite</button>
       <Outlet />
     </div>
   );
