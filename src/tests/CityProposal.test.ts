14dad4ff37aeedfe5f87427f82962b0c735090b2
Andreas Kjosavik <andrkjos@stud.ntnu.no>
2023-09-11 16:22:31 +0200

    refactor: move testing files
    Refs: #5

--- src/tests/CityProposal.test.ts
+++
@@ -1,5 +0,0 @@
-import CityProposal from "../components/CityProposal";
-import { render, screen, cleanUp } from "@testing-library/react";
-import { describe, it, expect } from "vitest";
-
-describe("CityProposal", () => {});
