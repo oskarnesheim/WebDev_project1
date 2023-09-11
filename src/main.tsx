25a230d576704485189ebe598ba0beb108ab5eb4
Oskar Nesheim <nesheimo@gmail.com>
2023-09-11 15:50:58 +0200

    add: showadvanced is stored locally

--- src/main.tsx
+++ src/main.tsx
@@ -29,10 +29,6 @@ const router = createBrowserRouter([
             element: <City />,
             children: [
               {
-                path: "current",
-                element: <CurrentWeather />,
-              },
-              {
                 path: "forecast",
                 element: <Forecast />,
               },
--- src/pages/City.tsx
+++ src/pages/City.tsx
@@ -1,40 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { useEffect, useState } from "react";
-import { NavLink, Outlet, useParams } from "react-router-dom";
-import { useRecoilState } from "recoil";
-import { favoriteCities } from "../recoil/atoms";
-import FavoritePreview from "../components/FavoritePreview";
+import { Outlet, useParams } from "react-router-dom";
+import CurrentWeather from "../components/CurrentWeather";
 
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 function City() {
   const { city } = useParams();
-  const [favoriteCitiesList, setFavoriteCitiesList] =
-    useRecoilState(favoriteCities);
-  const [starSymbol, setStarSymbol] = useState(getCityStatus(city!));
-
-  function getCityStatus(cityName: string) {
-    return !favoriteCitiesList.includes(cityName) ? "★" : "☆";
-  }
-
-  function toggleFavorite() {
-    const favorites: string[] = [...favoriteCitiesList];
-    if (!favorites.includes(city!)) {
-      favorites.push(city!);
-    } else {
-      favorites.splice(favorites.indexOf(city!), 1);
-    }
-    localStorage.setItem("favorites", JSON.stringify(favorites));
-    setFavoriteCitiesList(favorites);
-    setStarSymbol(getCityStatus(city!));
-  }
-
   return (
     <div>
-      <h2>
-        <div onClick={() => toggleFavorite()}>{starSymbol}</div>
-      </h2>
-      <FavoritePreview disaledLink={true} city={city!} />
+      <CurrentWeather city={city!} />
       <Outlet />
     </div>
   );
