4db683b31437285e59f6d08c35f3b30f924a1a51
Andreas Kjosavik <andrkjos@stud.ntnu.no>
2023-09-11 14:19:02 +0200

    feat: changed name and imports to be correct
    Refs: #5

--- src/tests/testFunction.test.ts
+++ src/tests/testFunction.test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from "vitest";
-import { sum } from "../functions/testFunction";
+import TestFunction from "../functions/TestFunction";
 
 test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+  expect(TestFunction(1, 2)).toBe(3);
 });
