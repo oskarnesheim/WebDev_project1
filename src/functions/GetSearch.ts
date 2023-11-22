494d2049e6ab935c21fd68c4797cfff38de1c67f
Oskar Nesheim <nesheimo@gmail.com>
2023-11-22 11:46:43 +0100

    cleanup removed a comment

--- src/functions/GetSearch.ts
+++ src/functions/GetSearch.ts
@@ -14,12 +14,3 @@ export async function getSearch(city: string): Promise<ICity[]> {
     .then((res) => res.data)
     .catch((err) => console.log(err));
 }
-// function getSearch(city: string) {
-//   const WEATHER_REQUEST =
-//     "http://api.weatherapi.com/v1/search.json?key=1413dd12c034448e8e894125230109&q=" +
-//     city;
-
-//   return getData(WEATHER_REQUEST);
-// }
-
-// export default getSearch;
